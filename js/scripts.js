caac4a03fda7ead85a0722383202a5c0a2db82d5
Virat <virat.algebraicgeometry@gmail.com>
2024-10-02 08:03:43 +0530

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,3 @@
-// script.js
-
-// Smooth scrolling for internal links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Virat's Math Website Loaded");
 });
-
-// Example welcome message
-window.onload = function() {
-    console.log("Welcome to Virat's Math Website!");
-};
